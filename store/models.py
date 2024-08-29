2ff09a73b8809f9f5b697b93690fa728447eed2a
Ankush <ankush.gupta.170802@gmail.com>
2024-08-30 01:07:33 +0530

    Added all views

--- store/models.py
+++ store/models.py
@@ -24,7 +24,7 @@ class Product(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    categories = models.ManyToManyField(Category, related_name='products')
+    categories = models.OneToOneField(Category, related_name='products',on_delete=models.CASCADE)
     collections = models.ManyToManyField(Collection, related_name='products', blank=True)
 
     def __str__(self):
@@ -34,7 +34,7 @@ class Image(models.Model):
     source = models.ImageField(upload_to='product_images/')
     alt_text = models.CharField(max_length=255, blank=True)
     updated_at = models.DateTimeField(auto_now=True)
-    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE,null=True)
 
     def __str__(self):
         return self.alt_text or self.source.name
@@ -46,7 +46,7 @@ class Variant(models.Model):
     available_for_sale = models.BooleanField(default=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     product = models.ForeignKey(Product, related_name='variants', on_delete=models.CASCADE)
-    image = models.ForeignKey(Image, related_name='variants', on_delete=models.CASCADE)
+    image = models.OneToOneField(Image, related_name='variants', on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.product.title} - {self.title}"
